dc4e78f9c99c930b86be64061d153fcc60f5ab78
Samuel Ryberg <samuel.ryberg@gmail.com>
2022-02-16 00:32:28 +0100

    Small improvements

---
+++ premake5.lua
@@ -0,0 +1,48 @@
+include "Premake/extensions.lua"
+
+workspace "CommonUtilities"
+	location "."
+	startproject "CommonUtilities"
+	architecture "x64"
+
+	configurations {
+		"Debug",
+		"Release"
+	}
+
+outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+project "CommonUtilities"
+	location "./CommonUtilities/"
+		
+	language "C++"
+	cppdialect "C++20"
+
+	targetdir ("./bin/%{prj.name}/" .. outputdir)
+	targetname("%{prj.name}-%{cfg.buildcfg}")
+	objdir ("./bin-int/%{prj.name}/" .. outputdir)
+
+	files {
+		"**.h",
+		"**.cpp"
+	}
+
+	includedirs {
+		"./CommonUtilities/",
+		"./CommonUtilities/include/"
+	}
+
+	filter "configurations:Debug"
+		defines "_DEBUG"
+		runtime "Debug"
+		symbols "on"
+		
+	filter "configurations:Release"
+		defines "_RELEASE"
+		runtime "Release"
+		optimize "on"
+
+	filter "system:windows"
+		kind "ConsoleApp"	
+		systemversion "latest"
+		
