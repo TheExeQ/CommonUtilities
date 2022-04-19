0d057c585f8ad0210ccdc9f9bb0bfc3bf18bf6ff
Samuel Ryberg <samuel.ryberg@gmail.com>
2022-04-19 10:43:22 +0200

    Update Queue.hpp

--- include/Queue.hpp
+++ include/Queue.hpp
@@ -1,56 +1,4 @@
 #pragma once
-<<<<<<< HEAD
-
-template <class T>
-class Queue
-{
-public:
-	Queue();
-
-	int GetSize() const;
-
-	const T& GetFront() const;
-	T& GetFront();
-
-	void Enqueue(const T& aValue);
-	T Dequeue();
-};
-
-template <class T>
-T Queue<T>::Dequeue()
-{
-
-}
-
-template <class T>
-void Queue<T>::Enqueue(const T& aValue)
-{
-
-}
-
-template <class T>
-T& Queue<T>::GetFront()
-{
-
-}
-
-template <class T>
-const T& Queue<T>::GetFront() const
-{
-
-}
-
-template <class T>
-int Queue<T>::GetSize() const
-{
-
-}
-
-template <class T>
-Queue<T>::Queue()
-{
-
-=======
 #include <vector>
 #include <cassert>
 
@@ -151,5 +99,4 @@ namespace CommonUtilities
 		myLast = 0;
 		mySize = 0;
 	}
->>>>>>> 90442be70fda61db8c7d970b5873eb3c862127b3
 }
